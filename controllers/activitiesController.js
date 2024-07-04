e04eb2fd739138e82f577043f10cafdb654f8961
deliphine <dephaingabire@gmail.com>
2024-07-04 11:17:32 +0300

    corrected error on get activities by staff

--- controllers/activitiesController.js
+++ controllers/activitiesController.js
@@ -16,12 +16,7 @@ export const getActivitiesByStaff = asyncCatch(async (req, res) => {
   const activities = await Activities.find({ staff: staffId }).populate(
     "staff"
   );
-  if (!activities || activities.length === 0) {
-    return res.status(404).json({
-      success: false,
-      message: "No activities found for this staff",
-    });
-  }
+  
   res.status(200).json({
     success: true,
     activities,
