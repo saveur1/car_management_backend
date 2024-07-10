967dfbab52c4a5efe2bf9bbb6cad54fcf5f13aa9
Saveur Bikorimana <bikorimanaxavier@gmail.com>
2024-07-10 16:43:09 +0200

    updated cors to contain only two cors

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -3,17 +3,7 @@ const sendToken = (statusCode,user,res)=>{
     //create jwt token
     const token = user.getJwtToken();
 
-    //options for cookie
-    const options = {
-        expires:new Date(
-            Date.now() + process.env.COOKIE_EXPIRES_TIME*24*60*60*1000
-        ),
-        secure:true,
-        sameSite:"None",
-        httpOnly:true
-    }
-
-    res.status(statusCode).cookie("token", token, options).json({
+    res.status(statusCode).json({
         success:true,
         token,
         user
