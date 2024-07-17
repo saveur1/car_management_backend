db00d74830276137af6a961697dd0f05a6911b3d
Saveur Bikorimana <bikorimanaxavier@gmail.com>
2024-07-17 16:21:11 +0200

    populated positions of staff model

--- controllers/activitiesController.js
+++ controllers/activitiesController.js
@@ -3,12 +3,22 @@ import asyncCatch from "../middlewares/asyncCatch.js";
 
 // Create a new asset
 export const createActivity = asyncCatch(async (req, res) => {
-    const activity = await Activities.create({
+    const newActivity = await Activities.create({
             ...req.body,
             activityName: "Added Activity",
             color: "blue",
             company: req.staff.company
     });
+
+    const activity = await Activities.findById(newActivity._id)
+                                     .populate("staff")
+                                        .populate({
+                                            path: "staff",
+                                            populate: {
+                                                path: "position",
+                                                model: "Job"
+                                            }
+                                        });
   
     res.status(201).json({
       status: "success",
@@ -19,7 +29,14 @@ export const createActivity = asyncCatch(async (req, res) => {
 // Get all activities
 export const getAllActivities = asyncCatch(async (req, res) => {
   const activities = await Activities.find({company: req.staff.company})
-                                     .populate("staff");
+                                     .populate("staff")
+                                     .populate({
+                                        path: "staff",
+                                        populate: {
+                                            path: "position",
+                                            model: "Job"
+                                        }
+                                     });
   res.status(200).json({
     success: true,
     activities,
@@ -30,7 +47,14 @@ export const getAllActivities = asyncCatch(async (req, res) => {
 export const getActivitiesByStaff = asyncCatch(async (req, res) => {
   const { staffId } = req.params;
   const activities = await Activities.find({ staff: staffId, company: req.staff.company })
-                                     .populate("staff");
+                                     .populate("staff")
+                                     .populate({
+                                        path: "staff",
+                                        populate: {
+                                            path: "position",
+                                            model: "Job"
+                                        }
+                                     });
   
   res.status(200).json({
     success: true,
