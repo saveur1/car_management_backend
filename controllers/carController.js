2ce26e7b8edf24e550712ec0d8880f3803a6ce75
Saveur Bikorimana <bikorimanaxavier@gmail.com>
2024-07-21 18:02:40 +0200

    populated supplier of cars

--- controllers/carController.js
+++ controllers/carController.js
@@ -8,7 +8,10 @@ import Activities from "../models/activityModel.js";
 
 //Register Car =>POST /api/v1/cars/create
 export const registerCar = asyncCatch(async(req,res,next)=>{
-  const car = await Car.create({...req.body, company: req.staff.company });
+  const newCar = await Car.create({...req.body, company: req.staff.company });
+
+  const car = await Car.findById(newCar._id)
+                         .populate("supplier");
 
   //add new activies
   await Activities.create({
@@ -28,6 +31,7 @@ export const registerCar = asyncCatch(async(req,res,next)=>{
 export const getAllCars = asyncCatch(async(req,res,next)=>{
 
     const cars = await Car.find({ company: req.staff.company })
+                          .populate("supplier")
                           .sort({createdAt: -1});
 
     res.status(200).json({
@@ -39,7 +43,8 @@ export const getAllCars = asyncCatch(async(req,res,next)=>{
 //get Car Details => Get /api/v1/cars/:id -> admin only route
 export const getCarDetails = asyncCatch(async(req,res,next)=>{
 
-    const car = await Car.findById(req.params.id);
+    const car = await Car.findById(req.params.id)
+                         .populate("supplier");
 
     if(!car){
         return next(new ErrorHandler(`Car with id ${req.params.id} is not found in database`,400))
@@ -55,10 +60,10 @@ export const getCarDetails = asyncCatch(async(req,res,next)=>{
 export const updateCarInfo = asyncCatch(async(req,res,next)=>{
 
   const car = await Car.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-    useFindAndModify: false,
-  });
+                        new: true,
+                        runValidators: true,
+                        useFindAndModify: false,
+                    }).populate("supplier");
 
   //update activies
   await Activities.create({
@@ -122,20 +127,20 @@ export const getCarCategory = asyncCatch(async(req,res,next)=>{
                 // manufacturer_year <= 2
                 manufacture_year:{ $gt: nowYear-2 },  // 2 represents the years car stays as new: stops at 2 years old: 1,2
                 company: req.staff.company
-            }).sort({createdAt: -1});
+            }).sort({createdAt: -1}).populate("supplier");
             break;
         case "medium":
             cars = await Car.find({
                 // 3 <= manufacturer_year <= 6
                 manufacture_year:{ $gte: nowYear-5, $lte: nowYear-2 }, //Start from 3 years old - 6 years old: 3,4,5
                 company: req.staff.company
-            }).sort({createdAt: -1});
+            }).sort({createdAt: -1}).populate("supplier");
             break;
         default:
             cars = await Car.find({
                 manufacture_year:{ $lt: nowYear-5 },  // start from 5 years old - Anywhere in past years: 6,7,8,9...
                 company: req.staff.company
-            }).sort({createdAt: -1});
+            }).sort({createdAt: -1}).populate("supplier");
             break;
     }
 
@@ -153,6 +158,7 @@ export const currentStatusCar = asyncCatch(async(req,res,next)=>{
 
     //get cars by current status
     const cars = await Car.find({current_status: query, company: req.staff.company})
+                          .populate("supplier")
                           .sort({createdAt: -1});
 
     res.status(200).json({
